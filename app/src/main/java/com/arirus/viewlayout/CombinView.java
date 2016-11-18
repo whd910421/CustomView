c5d64ca486987db12ac753b5b64ff3de418526de
Arirus <whd910421@hotmail.com>
2016-11-18 11:40:36 +0800

    h绘制弧线, 起始角度是3点种,旋转角度是以此顺时针,usecenter是是否要绘制圆心,STROKE类型是 绘制边线

--- app/src/main/java/com/arirus/viewlayout/CombinView.java
+++ app/src/main/java/com/arirus/viewlayout/CombinView.java
@@ -4,20 +4,32 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 
 /**
  * Created by whd910421 on 16/11/15.
  */
 
 public class CombinView extends View {
+    private int mMeasureHeigth;
     private int mMeasureWidth;
-    private int mMeasureHeigh;
 
+    private Paint mCirclePaint;
+    private float mCircleXY;
     private float mRadius;
-    private Paint mCriclePaint ;
+
+    private Paint mArcPaint;
+    private RectF mArcRectF;
+    private float mSweepAngle;
+    private float mSweepValue = 66;
+
+    private Paint mTextPaint;
+    private String mShowText;
+    private float mShowTextSize;
 
     public CombinView(Context context) {
         super(context);
@@ -35,22 +47,81 @@ public class CombinView extends View {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         //获取当前父窗口的大小
         mMeasureWidth = MeasureSpec.getSize(widthMeasureSpec);
-        mMeasureHeigh = MeasureSpec.getSize(heightMeasureSpec);
-        Log.i("ssssssss",String.valueOf(mMeasureWidth));
-        Log.i("ssssssss1",String.valueOf(mMeasureHeigh));
-        Log.i("ssssssss2",String.valueOf(getDefaultSize(getSuggestedMinimumWidth(), mMeasureWidth)));
-        Log.i("ssssssss2",String.valueOf(getDefaultSize(getSuggestedMinimumHeight(), mMeasureHeigh)));
-        setMeasuredDimension(mMeasureWidth,mMeasureHeigh);
+        mMeasureHeigth = MeasureSpec.getSize(heightMeasureSpec);
 
-        mRadius = (float) 0.5 * Math.min(mMeasureHeigh,mMeasureWidth);
-        mCriclePaint = new Paint();
-        mCriclePaint.setColor(Color.RED);
+        setMeasuredDimension(mMeasureWidth,mMeasureHeigth);
+
+        float shortlength = 0;
+        float longlength = 0;
+        if (mMeasureHeigth >= mMeasureWidth) {
+            shortlength = mMeasureWidth;
+            longlength = mMeasureHeigth;
+        } else {
+            shortlength = mMeasureHeigth;
+            longlength = mMeasureWidth;
+        }
+
+        mCircleXY = shortlength / 2;
+        mRadius = (float) (shortlength * 0.5 / 2);
+        mCirclePaint = new Paint();
+//        mCirclePaint.setAntiAlias(true);
+        mCirclePaint.setColor(getResources().getColor(
+                android.R.color.holo_blue_bright));
+        float dis = longlength- shortlength;
+        Log.i("ddddddd",String.valueOf(dis));
+        mArcRectF = new RectF(
+                (float) (shortlength * 0.1),
+                (float) (shortlength * 0.1),
+                (float) (shortlength * 0.9),
+                (float) (shortlength * 0.9));
+        mSweepAngle = (mSweepValue / 100f) * 360f;
+        mArcPaint = new Paint();
+        mArcPaint.setAntiAlias(true);
+        mArcPaint.setColor(getResources().getColor(
+                android.R.color.holo_blue_bright));
+        mArcPaint.setStrokeWidth((float) (shortlength * 0.1));
+        mArcPaint.setStyle(Paint.Style.STROKE);
+
+        mShowText = setShowText();
+        mShowTextSize = setShowTextSize();
+        mTextPaint = new Paint();
+        mTextPaint.setTextSize(mShowTextSize);
+        mTextPaint.setTextAlign(Paint.Align.CENTER);
+    }
+
+    private float setShowTextSize() {
+        this.invalidate();
+        return 50;
+    }
+
+    private String setShowText() {
+        this.invalidate();
+        return "Android Skill";
+    }
+
+    public void forceInvalidate() {
+        this.invalidate();
+    }
+
+    public void setSweepValue(float sweepValue) {
+        if (sweepValue != 0) {
+            mSweepValue = sweepValue;
+        } else {
+            mSweepValue = 25;
+        }
+        this.invalidate();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawCircle(mMeasureWidth/2, mMeasureHeigh/2, mRadius,mCriclePaint);
+        // 绘制圆
+//        canvas.drawCircle(mCircleXY, mCircleXY, mRadius, mCirclePaint);
+        // 绘制弧线
+        canvas.drawArc(mArcRectF, -90, 240, false, mArcPaint);
+        // 绘制文字
+        canvas.drawText(mShowText, 0, mShowText.length(),
+                mCircleXY, mCircleXY + (mShowTextSize / 4), mTextPaint);
     }
 }
